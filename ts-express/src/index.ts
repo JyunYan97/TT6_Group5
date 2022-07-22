6e1ffd81c099a852c70fcc278a2ffa895bad0aca
Kian Yee <kianyee.lim@hotmail.com>
2022-07-22 16:28:22 +0800

    fix(*): add cors

--- ts-express/src/index.ts
+++ ts-express/src/index.ts
@@ -2,13 +2,14 @@ import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import { connectDb } from './mongo'
 import routes from './routes'
+import { corsWithOptions } from './middleware/cors'
 
 const app: Application = express()
 const port = process.env.PORT || 3000
 dotenv.config()
 
 app.use(express.json())
-
+app.use(corsWithOptions)
 app.use('/api', routes)
 
 connectDb()
