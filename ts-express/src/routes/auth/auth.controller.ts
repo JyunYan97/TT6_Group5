70898f9b1a1184772065dd0c0df33c7ada797379
Kayden <89409516+k1any33@users.noreply.github.com>
2022-07-22 13:15:17 +0800

    feat(auth): add login and register endpoint (#3)

--- ts-express/src/routes/auth/auth.controller.ts
+++ ts-express/src/routes/auth/auth.controller.ts
@@ -3,10 +3,27 @@ import authService from './auth.service'
 
 const router = Router()
 
-router.post('/login', (req, res, next) => {
-  const loginDto = req.body
-  const result = authService.login()
-  return res.status(201).json(result)
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body
+  const response = await authService.login(email, password)
+
+  if (response.success) {
+    res.status(200).json(response.authToken)
+  }
+  // TODO: Handle exception better
+  res.status(400).json({ message: 'Something went wrong' })
+})
+
+router.post('/register', async (req, res, next) => {
+  const { email, password } = req.body
+  console.log(email, password)
+  const response = await authService.register(email, password)
+
+  if (response.success) {
+    res.status(200).json(response.authToken)
+  }
+  // TODO: Handle exception better
+  res.status(400).json({ message: 'Something went wrong' })
 })
 
 export default router
--- ts-express/src/routes/auth/auth.service.ts
+++ ts-express/src/routes/auth/auth.service.ts
@@ -1,3 +1,65 @@
-const login = () => {}
+import { userModel } from '../../mongo/user'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { AuthFail, AuthSuccess } from './auth.model'
 
-export default { login }
+const login = async (
+  email: string,
+  password: string
+): Promise<AuthSuccess | AuthFail> => {
+  try {
+    const existingUser = await userModel.findOne({ email })
+
+    if (!existingUser)
+      return { success: false, message: 'Email does not exist in DB' }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      existingUser.hashedPassword
+    )
+    if (!validPassword) return { success: false, message: 'Password incorrect' }
+
+    const tokenPayload = { id: existingUser._id }
+    const token = jwt.sign(tokenPayload, process.env.secretKey || 'secret', {
+      expiresIn: '7d',
+    })
+    return { success: true, authToken: token }
+  } catch (error) {
+    // TODO: logging
+    console.log(error)
+    return { success: false, message: error }
+  }
+}
+
+const register = async (
+  email: string,
+  password: string
+): Promise<AuthSuccess | AuthFail> => {
+  try {
+    const existingUser = await userModel.findOne({ email })
+
+    if (existingUser) return { success: false, message: 'Email already exist' }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    console.log(hashedPassword)
+
+    const newUser = await userModel.create({
+      email,
+      hashedPassword,
+    })
+    console.log(newUser)
+
+    const tokenPayload = { id: newUser._id }
+    const token = jwt.sign(tokenPayload, process.env.secretKey || 'secret', {
+      expiresIn: '7d',
+    })
+    return { success: true, authToken: token }
+  } catch (error) {
+    // TODO: logging
+    console.log(error)
+    return { success: false, message: error }
+  }
+}
+
+export default { login, register }
