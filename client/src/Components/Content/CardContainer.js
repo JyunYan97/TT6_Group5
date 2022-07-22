2c8c6a6a21c9c5fc45f3ed7641d6ec4b9bb6b305
luizly <108622654+luizly@users.noreply.github.com>
2022-07-22 16:54:03 +0800

    Update CardContainer.js
    Added the "add" button for wallet

--- client/src/Components/Content/CardContainer.js
+++ client/src/Components/Content/CardContainer.js
@@ -2,22 +2,37 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Cards from './Cards';
-
+import { Button } from 'react-bootstrap';
+import { useState } from 'react'
+ 
 function CardContainer() {
-  return (
-    <Container>
-      {/* <Row xs={1} md={2}>
-        <Col><Card /></Col>
-        <Col><Card /></Col>
-        
-      </Row> */}
-      <Row xs="auto" >
-        <Col style={{margin:'0.5rem'}}><Cards /></Col>
-        <Col style={{margin:'0.5rem'}}><Cards /></Col>
-        <Col style={{margin:'0.5rem'}}><Cards /></Col>
-      </Row>
-    </Container>
-  );
+ 
+ const [ addCard, setAddCard ] = useState(false)
+ 
+ return (
+ <>
+ <Container>
+ <Button onClick={(e)=>{
+ e.preventDefault()
+ setAddCard(true)}}>Add</Button>
+ {/* <Row xs={1} md={2}>
+ <Col><Card /></Col>
+ <Col><Card /></Col>
+ 
+ </Row> */}
+ <Row xs="auto" >
+ 
+ <Col style={{margin:'0.5rem'}}><Cards/></Col>
+ <Col style={{margin:'0.5rem'}}><Cards /></Col>
+ <Col style={{margin:'0.5rem'}}><Cards /></Col>
+ {
+ addCard ? <Col style={{margin:'0.5rem'}}><Cards /></Col> : <></>
+ }
+ </Row>
+ </Container>
+ </>
+ 
+ );
 }
-
+ 
 export default CardContainer;
