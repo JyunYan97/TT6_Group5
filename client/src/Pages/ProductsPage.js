780b6cc83014b15c845c024ab7ee06a3aeebe3e9
luizly <108622654+luizly@users.noreply.github.com>
2022-07-22 17:05:16 +0800

    Update ProductsPage.js
    Fully populated currency conversion table

--- client/src/Pages/ProductsPage.js
+++ client/src/Pages/ProductsPage.js
@@ -10,7 +10,7 @@ const Products = () => {
   return (
     <div>
         <NavBar1 />
-        <h1 style={{padding:'1rem'}}>Wallets</h1>
+        <h1 style={{padding:'1rem'}}>Products</h1>
         <Container>
         <Row>
         <Col><CardContainer /></Col>
@@ -29,7 +29,61 @@ const Products = () => {
  <tr>
  <td>CNH</td>
  <td>4.7868</td>
- </tr> 
+ </tr>
+ <tr>
+<td>CAD</td>
+<td>0.9925</td>
+</tr>
+
+<tr>
+<td>CCNH</td>
+<td>4.7868</td>
+</tr>
+
+<tr>
+<td>CEUR</td>
+<td>0.7086</td>
+</tr>
+
+<tr>
+<td>CHKD</td>
+<td>5.583</td>
+</tr>
+
+<tr>
+<td>JPY</td>
+<td>97.5303</td>
+</tr>
+
+<tr>
+<td>NZD</td>
+<td>1.1612</td>
+</tr>
+
+<tr>
+<td>NOK</td>
+<td>7.2912</td>
+</tr>
+
+<tr>
+<td>GBP</td>
+<td>0.5974</td>
+</tr>
+
+<tr>
+<td>SEK</td>
+<td>7.5168</td>
+</tr>
+
+<tr>
+<td>THB</td>
+<td>25.7275</td>
+</tr>
+
+<tr>
+<td>USD</td>
+<td>0.7113</td>
+</tr> 
  </tbody>
 </Table></Col>
         </Row>
