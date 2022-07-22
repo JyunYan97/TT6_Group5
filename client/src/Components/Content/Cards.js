6150ebb52541ab9fc6d4434e6abf9cb9c411b46e
luizly <108622654+luizly@users.noreply.github.com>
2022-07-22 15:14:39 +0800

    Update Cards.js

--- client/src/Components/Content/Cards.js
+++ client/src/Components/Content/Cards.js
@@ -2,30 +2,27 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useState } from 'react'
 
 function Cards() {
+  const [ chosenCur, setChosenCur ] = useState('SGD')
   const currency = ['CAD', 'CNH', 'EUR', 'HKD', 'JPY', 'NZD', 'NOK', 'GBP', 'SEK', 'THB', 'USD']
+  const [ amt, setAmt ] = useState(0)
+
   return (
     <Card style={{ width: '18rem'}}>
       {/* <Card.Img variant="top" src="https://media.istockphoto.com/photos/rear-view-of-family-walking-together-picture-id993354548?k=20&m=993354548&s=612x612&w=0&h=vM9qomjAJQ30PCn8-TleMuvsBa9cTN9ll_-apjwJdSs=" /> */}
       <Card.Body>
         <Card.Title>Wallet: Trip to Japan, Balance:___</Card.Title>
         <Card.Text>
-        
         <form>
-        
+        <select onChange={e=>setChosenCur(e.target.value)}>
+        {currency.map(money=><option>{money}</option>)} 
+        </select>
         <label htmlFor='Amount'>
-          Amount:  
+        Amount:  
         </label>
-        <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-        {currency.map( money => <Dropdown.Item>{money}</Dropdown.Item>)}
-        </Dropdown.Menu>
-        </Dropdown>
-        <input type="text" name="Amount" />
+        <input name="Amount" value={amt} type="number" onChange={e=>setAmt(e.target.value)}/>
         <Button variant="danger" type='submit'>Convert</Button>
         </form>
         </Card.Text>
